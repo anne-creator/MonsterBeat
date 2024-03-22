562cc2a6a4f859d88d129a83f676c0e1663d2458
Yan Liu <yliu2285@uwo.ca>
2024-03-22 19:19:36 -0400

    6 monsters and 6 characters img added

---
+++ src/backend/GameResult.java
@@ -0,0 +1,11 @@
+package backend;
+
+/**
+ * Useage: MarkCalculator.calculateMarks(monster, player, timeLeft)
+ */
+public class MarkCalculator {
+
+    public static int calculatMarks(Monster monster, Player player, int timeLeft) {
+        return 1; //testing
+    }
+}
